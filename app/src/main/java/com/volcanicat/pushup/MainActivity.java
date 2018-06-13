a3d50009a631fc65bb9dca7716657f5abfce4f9f
Alejandro <alejandrof.zamora@gmail.com>
2018-06-12 22:11:10 -0600

    second commit

--- app/src/main/java/com/volcanicat/pushup/MainActivity.java
+++ app/src/main/java/com/volcanicat/pushup/MainActivity.java
@@ -10,4 +10,9 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate( savedInstanceState );
         setContentView( R.layout.activity_main );
     }
+
+    private void prueba(){
+        int i =1+1;
+        int ii = 2+2;
+    }
 }
---
+++ app/src/main/java/com/volcanicat/pushup/test.java
@@ -0,0 +1,4 @@
+package com.volcanicat.pushup;
+
+public class test {
+}
